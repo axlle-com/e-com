0ddc877e93c58a6835a5d3ff14c99d95fc051766
axlle <axlle@mail.ru>
2022-04-25 21:15:32 +0300

    backend property type

--- resources/views/backend/catalog/inc/setting_tabs.blade.php
+++ resources/views/backend/catalog/inc/setting_tabs.blade.php
@@ -1,17 +1,11 @@
 <?php
 
-use App\Common\Models\Catalog\CatalogProduct;use App\Common\Models\Catalog\CatalogProductWidgets;use App\Common\Models\Catalog\CatalogProductWidgetsContent;
-
 /* @var $title string
- * @var $tabs CatalogProductWidgetsContent[]
- * @var $widget CatalogProductWidgets
- * @var $model CatalogProduct
  * @var $propertiesModel []
  * @var $properties []
  * @var $units []
  */
 ?>
-{{----}}
 
 <div class="catalog-tabs catalog-property">
     <div class="row">
@@ -33,7 +27,7 @@ use App\Common\Models\Catalog\CatalogProduct;use App\Common\Models\Catalog\Catal
                     'units' => $units,
                     'ids' => $ids,
                 ];
-                echo view('backend.catalog.inc.property',$data);
+                echo view('backend.catalog.inc.property', $data);
             }
         }
         ?>
