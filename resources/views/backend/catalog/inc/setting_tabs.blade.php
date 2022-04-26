4844dd5f55e3e3f86f22d90c6cf908f213edba15
Aleksei Alekseev <axlle@axlle.com>
2022-04-26 14:07:16 +0300

    Проперти

--- resources/views/backend/catalog/inc/setting_tabs.blade.php
+++ resources/views/backend/catalog/inc/setting_tabs.blade.php
@@ -5,6 +5,20 @@
  * @var $properties []
  * @var $units []
  */
+
+$string = '';
+if ($propertiesModel) {
+    $ids = $propertiesModel->pluck('property_id')->all();
+    foreach ($propertiesModel as $propertyModel) {
+        $data = [
+            'propertyModel' => $propertyModel,
+            'properties' => $properties,
+            'units' => $units,
+        ];
+        $string .= view('backend.catalog.inc.property', $data);
+    }
+}
+$ids = $ids ?? [];
 ?>
 
 <div class="catalog-tabs catalog-property">
@@ -12,24 +26,13 @@
         <div class="col-md-12">
             <button
                 type="button"
-                class="btn btn-primary catalog-tabs-add js-catalog-property-add">Добавить свойство
+                class="btn btn-primary catalog-tabs-add js-catalog-property-add"
+                data-js-properties-ids="<?= json_encode($ids) ?>">
+                Добавить свойство
             </button>
         </div>
     </div>
     <div class="row catalog-property-block">
-        <?php
-        if ($propertiesModel) {
-            $ids = $propertiesModel->pluck('property_id')->all();
-            foreach ($propertiesModel as $propertyModel) {
-                $data = [
-                    'propertyModel' => $propertyModel,
-                    'properties' => $properties,
-                    'units' => $units,
-                    'ids' => $ids,
-                ];
-                echo view('backend.catalog.inc.property', $data);
-            }
-        }
-        ?>
+        <?= $string ?>
     </div>
 </div>
